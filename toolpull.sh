3a4109331ff3c4db6e4672f5477ad07acfba4ed6
Sechord42 <157994689+Sechord42@users.noreply.github.com>
2024-01-29 10:18:44 -0500

    Update toolpull.sh
    check for docker installed already to prevent problems with install script running more than once.

--- toolpull.sh
+++ toolpull.sh
@@ -1,13 +1,31 @@
 # Pull down tools
+#!/bin/bash
 
 # -
 sudo apt update
 sudo apt upgrade
 
-# - Stage Docker
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
-rm ./get-docker.sh
+# Check if docker command is available
+if command -v docker &> /dev/null; then
+    echo "Docker is already installed."
+else
+    # Docker not found, attempt installation
+    echo "Docker not found. Attempting to install..."
+
+    # - Stage Docker
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sh get-docker.sh
+    rm ./get-docker.sh
+
+    # Check if installation was successful
+    if command -v docker &> /dev/null; then
+        echo "Docker installed successfully."
+    else
+        echo "Error: Docker installation failed."
+        exit 1
+    fi
+fi
+
 
 # - OWASP ZAP 
 echo 'deb http://download.opensuse.org/repositories/home:/cabelo/xUbuntu_22.10/ /' | sudo tee /etc/apt/sources.list.d/home:cabelo.list
